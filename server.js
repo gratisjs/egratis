b523e9f50a45e4b134dc859b67bf2dca258d8fc8
gratisjs <gaaa6870@gmail.com>
2025-05-22 11:09:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,13 +24,17 @@ app.use(express.json());
 // --- CONFIGURACIÓN DE LA CONEXIÓN A TU BASE DE DATOS MySQL ---
 // ¡¡IMPORTANTE!! Las credenciales se leerán de las variables de entorno de Render.
 // NO codifiques tus credenciales de base de datos directamente aquí en producción.
+
 const dbConfig = {
-    host: process.env.DB_DATABASE,         // Variable de entorno de Render
-    user: process.env.DB_HOST,         // Variable de entorno de Render
-    password: process.env.DB_PASSWORD, // Variable de entorno de Render
-    database: process.env.DB_PORT 
-     database: process.env.DB_USER // Variable de entorno de Render
+    host: process.env.DB_HOST,         // Variable de entorno de Render
+    user: process.env.DB_USER,        // Variable de entorno de Render
+    password: process.env.DB_PASSWORD,  // Variable de entorno de Render
+    database: process.env.DB_DATABASE,    // Variable de entorno de Render
+    port: process.env.DB_PORT        // Variable de entorno de Render
 };
+
+
+
 
 let dbPool; // Usaremos un pool de conexiones para mayor eficiencia y rendimiento
 
